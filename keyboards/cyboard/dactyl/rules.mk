28545431e06ca038fd52428529417fd6b55645e9
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-04-30 00:56:44 -0400

    Move non-rgb rules.mk features to info.json

--- keyboards/cyboard/dactyl/rules.mk
+++ keyboards/cyboard/dactyl/rules.mk
@@ -1,20 +1,6 @@
-# MCU name
-MCU = RP2040
-
-# Bootloader selection
-BOOTLOADER = rp2040
-
 # Build Options
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-NKRO_ENABLE = no            # USB Nkey Rollover
 
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
 RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
